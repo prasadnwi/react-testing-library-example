5008ad853130a515dce0afdcf40f2cde8c735cbf
isuru madhuwantha <prasadnwip@gmail.com>
2025-01-20 16:53:20 +0530

    added a another test case for increment button

--- src/Counter.test.js
+++ src/Counter.test.js
@@ -1,5 +1,5 @@
 import Counter from "./Counter";
-import {render} from "@testing-library/react";
+import {fireEvent, render} from "@testing-library/react";
 
 describe(Counter,  () => {
     it("counter displays correct initial count", () => {
@@ -8,8 +8,14 @@ describe(Counter,  () => {
         expect(countValue).toEqual(0);
     });
     it("count should increment by 1 if the increment button is clicked", () => {
-        const {getByTestId} = render(<Counter initialCount={0} />);
+        const {getByTestId, getByRole} = render(<Counter initialCount={0} />);
+        const incrementBtn = getByRole("button", {name: "Increment"})
+        fireEvent.click(incrementBtn);
+
         const countValue = Number(getByTestId("count").textContent);
-        expect(countValue).toEqual(0);
+        expect(countValue).toEqual(1);
+        fireEvent.click(incrementBtn);
+        const countValue2 = Number(getByTestId("count").textContent);
+        expect(countValue2).toEqual(2);
     })
 })
